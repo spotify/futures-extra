41556cf244479ce596d5c0b9ebc9d0060ff5a7be
Kristofer Karlsson <krka@spotify.com>
2013-08-27 17:19:03 +0200

    Simplify the code enormously
    Change-Id: I3949a2273fa13bea4c28ac93bd021ee45e7bdb8a
    Reviewed-on: https://gerrit.spotify.net/gerrit/72545
    TC-Compile-And-Test: Team City <teamcity@spotify.com>
    Reviewed-by: Rouzbeh Delavari <rouz@spotify.com>
    Reviewed-by: Kristofer Karlsson <krka@spotify.com>

--- src/main/java/com/spotify/futures/FuturesExtra.java
+++ src/main/java/com/spotify/futures/FuturesExtra.java
@@ -1,22 +1,21 @@
 package com.spotify.futures;
 
-import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.base.Function;
+import com.google.common.util.concurrent.AsyncFunction;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.SettableFuture;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class FuturesExtra {
   public static <Z, A, B> ListenableFuture<Z> transform(ListenableFuture<A> a,
                                                         ListenableFuture<B> b,
                                                         final Function2<Z, A, B> function) {
-    return transform(Arrays.asList(a, b), new Function<Z>() {
+    return transform(Arrays.asList(a, b), new Function<List<Object>, Z>() {
       @Override
-      public Z apply(Object[] results) {
-        return function.apply((A) results[0], (B) results[1]);
+      public Z apply(List<Object> results) {
+        return function.apply((A) results.get(0), (B) results.get(1));
       }
     });
   }
@@ -29,10 +28,10 @@ public class FuturesExtra {
           ListenableFuture<A> a,
           ListenableFuture<B> b,
           final AsyncFunction2<Z, A, B> function) {
-    return transform(Arrays.asList(a, b), new AsyncFunction<Z>() {
+    return transform(Arrays.asList(a, b), new AsyncFunction<List<Object>, Z>() {
       @Override
-      public ListenableFuture<Z> apply(Object[] results) {
-        return function.apply((A) results[0], (B) results[1]);
+      public ListenableFuture<Z> apply(List<Object> results) {
+        return function.apply((A) results.get(0), (B) results.get(1));
       }
     });
   }
@@ -46,10 +45,10 @@ public class FuturesExtra {
           ListenableFuture<B> b,
           ListenableFuture<C> c,
           final Function3<Z, A, B, C> function) {
-    return transform(Arrays.asList(a, b, c), new Function<Z>() {
+    return transform(Arrays.asList(a, b, c), new Function<List<Object>, Z>() {
       @Override
-      public Z apply(Object[] results) {
-        return function.apply((A) results[0], (B) results[1], (C) results[2]);
+      public Z apply(List<Object> results) {
+        return function.apply((A) results.get(0), (B) results.get(1), (C) results.get(2));
       }
     });
   }
@@ -63,10 +62,10 @@ public class FuturesExtra {
           ListenableFuture<B> b,
           ListenableFuture<C> c,
           final AsyncFunction3<Z, A, B, C> function) {
-    return transform(Arrays.asList(a, b, c), new AsyncFunction<Z>() {
+    return transform(Arrays.asList(a, b, c), new AsyncFunction<List<Object>, Z>() {
       @Override
-      public ListenableFuture<Z> apply(Object[] results) {
-        return function.apply((A) results[0], (B) results[1], (C) results[2]);
+      public ListenableFuture<Z> apply(List<Object> results) {
+        return function.apply((A) results.get(0), (B) results.get(1), (C) results.get(2));
       }
     });
   }
@@ -81,10 +80,10 @@ public class FuturesExtra {
           ListenableFuture<C> c,
           ListenableFuture<D> d,
           final Function4<Z, A, B, C, D> function) {
-    return transform(Arrays.asList(a, b, c, d), new Function<Z>() {
+    return transform(Arrays.asList(a, b, c, d), new Function<List<Object>, Z>() {
       @Override
-      public Z apply(Object[] results) {
-        return function.apply((A) results[0], (B) results[1], (C) results[2], (D) results[3]);
+      public Z apply(List<Object> results) {
+        return function.apply((A) results.get(0), (B) results.get(1), (C) results.get(2), (D) results.get(3));
       }
     });
   }
@@ -99,10 +98,10 @@ public class FuturesExtra {
           ListenableFuture<C> c,
           ListenableFuture<D> d,
           final AsyncFunction4<Z, A, B, C, D> function) {
-    return transform(Arrays.asList(a, b, c, d), new AsyncFunction<Z>() {
+    return transform(Arrays.asList(a, b, c, d), new AsyncFunction<List<Object>, Z>() {
       @Override
-      public ListenableFuture<Z> apply(Object[] results) {
-        return function.apply((A) results[0], (B) results[1], (C) results[2], (D) results[3]);
+      public ListenableFuture<Z> apply(List<Object> results) {
+        return function.apply((A) results.get(0), (B) results.get(1), (C) results.get(2), (D) results.get(3));
       }
     });
   }
@@ -118,10 +117,10 @@ public class FuturesExtra {
           ListenableFuture<D> d,
           ListenableFuture<E> e,
           final Function5<Z, A, B, C, D, E> function) {
-    return transform(Arrays.asList(a, b, c, d, e), new Function<Z>() {
+    return transform(Arrays.asList(a, b, c, d, e), new Function<List<Object>, Z>() {
       @Override
-      public Z apply(Object[] results) {
-        return function.apply((A) results[0], (B) results[1], (C) results[2], (D) results[3], (E) results[4]);
+      public Z apply(List<Object> results) {
+        return function.apply((A) results.get(0), (B) results.get(1), (C) results.get(2), (D) results.get(3), (E) results.get(4));
       }
     });
   }
@@ -137,10 +136,10 @@ public class FuturesExtra {
           ListenableFuture<D> d,
           ListenableFuture<E> e,
           final AsyncFunction5<Z, A, B, C, D, E> function) {
-    return transform(Arrays.asList(a, b, c, d, e), new AsyncFunction<Z>() {
+    return transform(Arrays.asList(a, b, c, d, e), new AsyncFunction<List<Object>, Z>() {
       @Override
-      public ListenableFuture<Z> apply(Object[] results) {
-        return function.apply((A) results[0], (B) results[1], (C) results[2], (D) results[3], (E) results[4]);
+      public ListenableFuture<Z> apply(List<Object> results) {
+        return function.apply((A) results.get(0), (B) results.get(1), (C) results.get(2), (D) results.get(3), (E) results.get(4));
       }
     });
   }
@@ -149,57 +148,11 @@ public class FuturesExtra {
     ListenableFuture<Z> apply(A a, B b, C c, D d, E e);
   }
 
-  private static <Z> ListenableFuture<Z> transform(final List<ListenableFuture<?>> inputs, final Function<Z> function) {
-    return transform(inputs, new AsyncFunction<Z>() {
-      @Override
-      public ListenableFuture<Z> apply(Object[] results) {
-        return Futures.immediateFuture(function.apply(results));
-      }
-    });
+  private static <Z> ListenableFuture<Z> transform(final List<ListenableFuture<?>> inputs, final Function<List<Object>, Z> function) {
+    return Futures.transform(Futures.allAsList(inputs), function);
   }
 
-  private static <Z> ListenableFuture<Z> transform(final List<ListenableFuture<?>> inputs, final AsyncFunction<Z> function) {
-    final SettableFuture<Z> result = SettableFuture.create();
-    final Object[] values = new Object[inputs.size()];
-    final AtomicInteger countdown = new AtomicInteger(inputs.size());
-    for (int i = 0; i < inputs.size(); i++) {
-      final int finalI = i;
-      ListenableFuture<?> input = inputs.get(finalI);
-      Futures.addCallback(input, new FutureCallback<Object>() {
-        @Override
-        public void onSuccess(Object o) {
-          values[finalI] = o;
-          if (countdown.decrementAndGet() == 0) {
-            ListenableFuture<Z> newFuture = function.apply(values);
-            Futures.addCallback(newFuture, new FutureCallback<Z>() {
-              @Override
-              public void onSuccess(Z z) {
-                result.set(z);
-              }
-
-              @Override
-              public void onFailure(Throwable throwable) {
-                result.setException(throwable);
-              }
-            });
-          }
-        }
-
-        @Override
-        public void onFailure(Throwable throwable) {
-          result.setException(throwable);
-        }
-      });
-    }
-    return result;
+  private static <Z> ListenableFuture<Z> transform(final List<ListenableFuture<?>> inputs, final AsyncFunction<List<Object>, Z> function) {
+    return Futures.transform(Futures.allAsList(inputs), function);
   }
-
-  private static interface Function<Z> {
-    Z apply(Object[] results);
-  }
-
-  private static interface AsyncFunction<Z> {
-    ListenableFuture<Z> apply(Object[] results);
-  }
-
 }
