34c0d5271e19dfa4668a194e29eba8b763672717
Kristofer Karlsson <krka@spotify.com>
2018-08-16 11:18:07 +0200

    Remove unnecessary package structure

--- src/test/java/com/spotify/futures/JoinedResultsTest.java
+++ src/test/java/com/spotify/futures/JoinedResultsTest.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.spotify.futures.jdk7;
+package com.spotify.futures;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
