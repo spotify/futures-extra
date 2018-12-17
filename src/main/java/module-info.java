838f8ae459c4d0648af71a464a3e5d11873cb997
Stanislav Baiduzhyi <baiduzhyi.devel@gmail.com>
2018-12-17 16:13:12 +0100

    JPMS support for real. (#35)
    * JPMS support for real.
    
    The recommended way to make a library JPMS-compatible, yet preserve backward-compatibility with Java 8.
    
    Includes following changes:
    
      1. building with java 11, then re-building with java 8 except for module-info.
      2. javadoc Xdoclint is not supported, replaced with failOnError=false.
      3. checkstyle does not support module-info yet, excluded.
      4. since the library can be used for any of working with CompletionStage, ApiFuture and ListenableFuture, the module declares them as compile-time dependencies (maven changed accordingly).
    
    * Temporary disabling checkstyle.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+module com.spotify.futures {
+  /*
+   * com.google.guava:guava,
+   * dependency is optional in case only CompletionStage
+   * and/or ApiFuture are required:
+   */
+  requires static com.google.common;
+  /*
+   * com.google.api:api-common,
+   * dependency is optional in case only CompletionStage
+   * and/or ListenableFuture are required:
+   */
+  requires static api.common;
+}
